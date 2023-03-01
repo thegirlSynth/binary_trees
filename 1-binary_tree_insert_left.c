1444ee8808fd3ee84c5c8961bb549c34744f4bc2
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-01 19:13:33 +0100

    fixed task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,6 +24,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->left = parent->left;
 	new->right = NULL;
 
+	if (parent->left)
+		parent->left->parent = new;
+
 	parent->left = new;
 
 	return (new);
