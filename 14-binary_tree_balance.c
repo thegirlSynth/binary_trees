313333cfb0c0549625bf29c4913ff5a527ee3c46
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-01 20:20:45 +0100

    task 16

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,7 +21,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (factor);
 }
 
-
+/**
+ * binary_tree_height - finds the height of a binary tree
+ *
+ * @tree: the tree
+ *
+ * Return: the height
+ */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
