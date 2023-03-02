63090766c2d58f58251ddbd977715ae99f60515e
Cynthia Uche <cynthianuchepro@gmail.com>
2023-03-02 14:12:04 +0100

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,14 +9,14 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t tree_depth = 0;
+	size_t tree_depth = 0;
 
-    if (tree == NULL)
-        return (0);
-    while (tree->parent != NULL)
-    {
-        tree_depth++;
-        tree = tree->parent;
-    }
-    return (tree_depth);
+	if (tree == NULL)
+		return (0);
+	while (tree->parent != NULL)
+	{
+	tree_depth++;
+	tree = tree->parent;
+	}
+	return (tree_depth);
 }
