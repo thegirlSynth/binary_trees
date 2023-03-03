b7e60cf9a5b2bf559eac3909f20e48cc4dfd4d2b
Cynthia Uche <cynthianuchepro@gmail.com>
2023-03-03 08:37:39 -0800

    Fix Betty Code Style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -68,12 +68,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	int left, right;
 
 	if (tree == NULL)
-   		return (0);
+		return (0);
 
 	left = binary_tree_is_full(tree->left);
 	right = binary_tree_is_full(tree->right);
 
 	if (left == right)
-    	return (1);
+		return (1);
 	return (0);
 }
